58de888bac9301a5204cb0bca41eaa3052941da3
Maxym <maxymbonnette@gmail.com>
2018-11-30 16:22:22 -0500

    Implémentation de get Word

--- wordListDefinition.java
+++ wordListDefinition.java
@@ -6,6 +6,8 @@ import java.util.Vector;
 public class wordListDefinition  implements InterfaceWordDefinition{
 
     private List<WordDefinition> wordListDictio;
+    private int positionCharWord;
+    private String researchWord;
 
     public wordListDefinition(){
         setWordListDictio(new Vector<WordDefinition>());
@@ -16,6 +18,23 @@ public class wordListDefinition  implements InterfaceWordDefinition{
         this.wordListDictio = wordListDictio;
     }
 
+    private List<WordDefinition> searchWord(char c, List<WordDefinition> remaningWord){
+        List<WordDefinition> listToSearch = new Vector<WordDefinition>();
+        listToSearch = remaningWord;
+        for(WordDefinition word : listToSearch){
+            if(word.getWord().length() - 1 <= positionCharWord){
+                if (word.getWord().charAt(positionCharWord) != c)
+                    remaningWord.remove(word);
+            }else{
+                remaningWord.remove(word);
+            }
+        }
+        if(researchWord.length() - 1 > positionCharWord) {
+            positionCharWord++;
+            searchWord(researchWord.charAt(positionCharWord), listToSearch);
+        }
+        return listToSearch;
+    }
     @Override
     public boolean addWordDefinition(WordDefinition m) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -28,7 +47,9 @@ public class wordListDefinition  implements InterfaceWordDefinition{
 
     @Override
     public List<WordDefinition> getWord(String m) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        positionCharWord = 0;
+        researchWord = m;
+        return searchWord(researchWord.charAt(positionCharWord),wordListDictio);
     }
 
     @Override
